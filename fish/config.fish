8ebca6b8adc6d99afe3f41a865a59cc47c541dc1
ALNR-zshrc <zumasevalinur@gmail.com>
2026-04-03 18:52:15 +0600

    добавил новые alias

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,18 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -gx EDITOR nvim
 
 # Навигация
-abbr -a cd zoxide
 abbr -a ~ 'cd ~'
 abbr -a .. 'cd ..'
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
 
 # Утилиты
 abbr -a cl clear
-abbr -a ls 'eza --icons'
 abbr -a cat bat
+
 
 # Работа с пакетами 
 abbr -a update "sudo pacman -Syu"
